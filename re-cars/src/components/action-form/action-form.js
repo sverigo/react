b3368f4bfe7de5af15827879690611827a4b510c
Stanislav Veriho <sveri@macmini4356.ddns.softservecom.com>
2019-04-16 12:30:45 +0300

    added onBlur event

--- re-cars/src/components/action-form/action-form.js
+++ re-cars/src/components/action-form/action-form.js
@@ -13,18 +13,21 @@ const ActionForm = (props) => {
             <form onSubmit={handleSubmit}>
                 <TextField margin="normal" name="make" label="Make" fullWidth
                     onChange={handleChange}
+                    onBlur={handleChange}
                     value={state.make}
                     error={Boolean(state.validateErrors.make)}
                     helperText={state.validateErrors.make}
                 />
                 <TextField margin="normal" name="model" label="Model" fullWidth
                     onChange={handleChange}
+                    onBlur={handleChange}
                     value={state.model}
                     error={Boolean(state.validateErrors.model)}
                     helperText={state.validateErrors.model}
                 />
                 <TextField margin="normal" name="price" label="Price" fullWidth
                     onChange={handleChange}
+                    onBlur={handleChange}
                     value={state.price}
                     error={Boolean(state.validateErrors.price)}
                     helperText={state.validateErrors.price}
